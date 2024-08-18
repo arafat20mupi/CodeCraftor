c0119fe7b092f88d20f1ac03ea32aec40e3f558c
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-08-19 02:07:25 +0600

    error page added

---
+++ src/component/Pages/Error/Error.jsx
@@ -0,0 +1,10 @@
+import errorImg from '../../../assets/images/error.png';
+const Error = () => {
+    return (
+        <div className='flex items-center justify-center' >
+            <img src={errorImg} alt="Error" />
+        </div>
+    );
+};
+
+export default Error;
--- src/component/router/router.jsx
+++ src/component/router/router.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Pages/Main/Main";
 import Home from "../Pages/Home/Home";
+import Error from "../Pages/Error/Error";
 
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
